14227fed4f041e894e4546832b1f8862dc611f8f
liangchengcheng <1038127753@qq.com>
2020-03-03 10:56:14 +0800

    对部分代码规整1

--- app-service-eureka/src/main/java/com/hdsx/appservice/EurekaServerApplication.java
+++ app-service-eureka/src/main/java/com/hdsx/appservice/EurekaServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 梁铖城
+ * 2020年03月03日10:55:24
+ * 服务注册中心
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
